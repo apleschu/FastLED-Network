96b0a8f84311afa1783ec5bdea221871e171a766
Andreas Pleschutznig <andy@ple.org>
2016-04-07 08:01:08 -0500

    Initial Commit

---
+++ FastLED/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FastLED
+//
+//  Created by Andreas Pleschutznig on 4/7/16.
+//  Copyright © 2016 Andreas Pleschutznig. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
